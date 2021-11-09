56f0889bb0d626370baaf7b0e03794e43755cd36
Fallest <56403805+Fallest@users.noreply.github.com>
2021-11-09 21:20:08 +0100

    Update Fondo.cs

--- DEINT/C#/GestorClub/GestorClub/GestorClub/Objetos/Fondo.cs
+++ DEINT/C#/GestorClub/GestorClub/GestorClub/Objetos/Fondo.cs
@@ -2,6 +2,26 @@
 
 namespace GestorClub.Objetos {
 class Fondo {
+    // Array principal, que al inicio tiene nada más que 10 posiciones.
+    private Ejemplar[] _fondo;
+
+    /*-------------------------------------------------------------------------------*/
+    // Constructor
+    public Fondo() {
+        _fondo = new Ejemplar[10];
+    }
     
+    /*-------------------------------------------------------------------------------*/
+    // Operaciones añadir y eliminar.
+    public void add(Ejemplar e) {
+        throw new NotImplementedException();
+    }
+
+    public void remove(string titulo, string tipo) {
+        throw new NotImplementedException();
+    }
+    
+    /*-------------------------------------------------------------------------------*/
+    // Operacion para ordenar.
 }
 }
