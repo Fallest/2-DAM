21bd6b4d9e3e8bc2a416d155356e1d67594b1481
Fallest <56403805+Fallest@users.noreply.github.com>
2022-02-08 18:33:39 +0100

    PSP
    PSP:
    *Empezado el ejercicio 1.
    Empresas:
    +Añadidos algunos datos para el plan.
    
    D

--- PSP/ComunicacionRed/Boletin3_ComunicacionesRed/src/Ejercicio1/Persona.java
+++ PSP/ComunicacionRed/Boletin3_ComunicacionesRed/src/Ejercicio1/Persona.java
@@ -1,5 +1,36 @@
 package Ejercicio1;
 
-public class Persona {
-    
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class Persona implements Serializable {
+
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad) {
+        super();
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Persona() {
+        super();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
--- PSP/ComunicacionRed/Boletin3_ComunicacionesRed/src/Ejercicio1/Server.java
+++ PSP/ComunicacionRed/Boletin3_ComunicacionesRed/src/Ejercicio1/Server.java
@@ -1,5 +1,27 @@
 package Ejercicio1;
 
+import java.io.*;
+import java.net.*;
+
 public class Server {
+    /**
+     * Server TCP
+     */
     
+    public static void main(String[] args) {
+        try {
+            int port = 6000;
+
+            ServerSocket server = new ServerSocket(port);
+            System.out.println("Server is currently listening for calls at port "
+                    + server.getLocalPort());
+
+            Socket client = server.accept();
+            
+            client.close();
+            server.close();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
 }
