6a4a06b1a608f560a0b409c94945ca7ca17f9409
Fallest <56403805+Fallest@users.noreply.github.com>
2022-02-24 16:56:43 +0100

    ADAT
    +Añadido enunciado de la práctica 2.
    +Modificado el script DropTriggers: Ahora tiene un comando para eliminar las tablas.
    +Creados los objetos y tablas de la BD.
    
    D

--- ADAT/DropTriggers.sql
+++ ADAT/DropTriggers.sql
@@ -3,3 +3,9 @@ begin
 		execute immediate 'drop trigger ' || i.owner || '.' || i.trigger_name;
 	end loop;
 end;
+
+begin 
+	for i in (select table_name from user_tables) loop
+		execute immediate 'drop table ' || i.table_name || ' cascade constraints';
+	end loop;
+end;
