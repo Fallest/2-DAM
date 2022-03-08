f7083d0448041b325de6fb0796b01dea6ba736b1
Fallest <56403805+Fallest@users.noreply.github.com>
2022-03-08 20:12:36 +0100

    DEINT
    *Modificación de la práctica de PyWinAuto.
    
    D

--- DEINT/AutoTest/CalcTest.py
+++ DEINT/AutoTest/CalcTest.py
@@ -8,35 +8,51 @@ from time import sleep
 def runCalc():
     app = Application(backend="uia").start('calc.exe')
     dlg = Desktop(backend="uia").Calculator
-    dlg.print_control_identifiers()
 
     log = open('log.txt', 'w', encoding='utf-8')
 
     # Comprobación de la suma
     suma = "Sumando 23 + 39 => Resultado esperado: 62 | Resultado obtenido: "
     dlg.type_keys('23{+}39=')
-    suma += dlg.Static3.texts()[0].split(' ')[3] + "\n"
+    resSuma = int(dlg.Static3.texts()[0].split(' ')[3])
+    suma += str(resSuma) + " => CORRECTO\n" if 62 == resSuma else " => INCORRECTO\n"
     print(suma)
+    sleep(1)
 
     # Comprobación de la resta
     resta = "Restando 23 - 39 => Resultado esperado: -16 | Resultado obtenido: "
     dlg.type_keys('23{-}39=')
-    resta += dlg.Static3.texts()[0].split(' ')[3] + "\n"
+    resResta = int(dlg.Static3.texts()[0].split(' ')[3])
+    resta += str(resResta) + " => CORRECTO\n" if -16 == resResta else " => INCORRECTO\n"
     print(resta)
+    sleep(1)
 
     # Comprobación de la multiplicación
     mult = "Multiplicando 23 * 39 => Resultado esperado: 897 | Resultado obtenido: "
     dlg.type_keys('23{*}39=')
-    mult += dlg.Static3.texts()[0].split(' ')[3] + "\n"
+    resMult = int(dlg.Static3.texts()[0].split(' ')[3])
+    mult += str(resMult) + " => CORRECTO\n" if 897 == resMult else " => INCORRECTO\n"
     print(mult)
+    sleep(1)
 
     # Comprobación de la división
-    div = "dividiendo 23 / 39 => Resultado esperado: 0,589743... | Resultado obtenido: "
+    div = "Dividiendo 23 / 39 => Resultado esperado: 0,589743 | Resultado obtenido: "
     dlg.type_keys('23{/}39=')
-    div += dlg.Static3.texts()[0].split(' ')[3] + "\n"
+    resDiv = float(dlg.Static3.texts()[0].split(' ')[3].replace(',', '.')[0:8])
+    div += str(resDiv) + " => CORRECTO\n" if 0.589743 == resDiv else " => INCORRECTO\n"
     print(div)
+    sleep(1)
 
-    log.writelines([suma, resta, mult, div])
+    # Comprobación de la división por 0
+    div0 = "Dividiendo 23 / 0 => Resultado esperado: No se puede dividir entre cero | Resultado obtenido: "
+    dlg.type_keys('23{/}0=')
+    resDiv0 = ' '.join(dlg.Static3.texts()[0].split(' ')[3:])
+    div0 += str(resDiv0) +\
+            " => CORRECTO\n" if "No se puede dividir entre cero" == resDiv0 else " => INCORRECTO\n"
+    print(div0)
+    sleep(1)
+
+    log.writelines([suma, resta, mult, div, div0])
 
     log.close()
 
--- DEINT/AutoTest/NotepadTest.py
+++ DEINT/AutoTest/NotepadTest.py
@@ -9,43 +9,59 @@ def runNotepad():
     app = Application().start('notepad.exe')
 
     # Creamos la cadena con las instrucciones
-    text = "Este programa va a realizar las siguientes acciones:\n"\
-           "1. Mostrar la versión de Notepad.\n" \
-           "2. Escribir una cita célebre.\n" \
-           "3. Tomar una captura del texto y guardarla como captura.png\n" \
-           "4. Guardar el texto en texto.txt\n" \
-           "5. Cerrar el programa.\n\n"
+    text: list(str) = ["Este programa va a realizar las siguientes acciones:\n",
+           "\t1. Mostrar la versión de Notepad.\n" ,
+           "\t2. Escribir una cita célebre.\n" ,
+           "\t3. Tomar una captura del texto y guardarla como captura.png\n" ,
+           "\t4. Guardar el texto en texto.txt\n" ,
+           "\t5. Cerrar el programa.\n\n"]
 
-    cita = "\tEl cielo está destinado a crecer."
-
-    # Escribe las instrucciones
-    app.UntitledNotepad.Edit.type_keys(text,
-                                       with_spaces=True,
-                                       with_newlines=True)
+    cita = "\t\tEl cielo está destinado a crecer.\n"
 
     # 1. Mostrar la versión
+    app.UntitledNotepad.Edit.type_keys(text[0],
+                                       with_spaces=True,
+                                       with_newlines=True)
+    app.UntitledNotepad.Edit.type_keys(text[1],
+                                       with_spaces=True,
+                                       with_newlines=True)
     app.UntitledNotepad.menu_select("Ayuda -> Acerca del Bloc de notas")
     sleep(3)
     app.AcercaDelBlocDeNotas.Aceptar.click()
 
+
     # 2. Escribir una cita célebre
-    sleep(1)
+    app.UntitledNotepad.Edit.type_keys(text[2],
+                                       with_spaces=True,
+                                       with_newlines=True)
+    sleep(2)
     app.UntitledNotepad.Edit.type_keys(cita,
                                        with_spaces=True,
                                        with_newlines=True,
                                        with_tabs=True)
 
     # 3. Tomar la captura y guardarla
-    sleep(1)
+    app.UntitledNotepad.Edit.type_keys(text[3],
+                                       with_spaces=True,
+                                       with_newlines=True)
+    sleep(2)
     app.UntitledNotepad.capture_as_image().save('captura.png')
 
     # 4. Guarda el archivo
-    sleep(1)
+    app.UntitledNotepad.Edit.type_keys(text[4],
+                                       with_spaces=True,
+                                       with_newlines=True)
+    sleep(2)
     app.UntitledNotepad.menu_select("Archivo -> Guardar")
     app.GuardarComo.Edit.type_keys("texto.txt")
     app.GuardarComo.Guardar.click()
 
     # 5. Cerrar el programa
+    app.UntitledNotepad.Edit.type_keys(text[5],
+                                       with_spaces=True,
+                                       with_newlines=True)
+    sleep(2)
+    app.UntitledNotepad.menu_select("Archivo -> Guardar")
     app.UntitledNotepad.menu_select("Archivo -> Salir")
 
-#runNotepad()
+runNotepad()
