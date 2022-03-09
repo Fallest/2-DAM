636c72a5b703bed4dd95e97ae123dbdfa02b8e10
Fallest <56403805+Fallest@users.noreply.github.com>
2022-03-09 20:02:29 +0100

    SGEMP
    nosedeverdadsoloquieromorirya
    
    D

--- PSP/Tema 3/EscanerPuertos/src/Main/PortScanner.java
+++ PSP/Tema 3/EscanerPuertos/src/Main/PortScanner.java
@@ -6,7 +6,7 @@ import java.net.*;
 public class PortScanner {
     public static void main(String[] args) {
         System.out.println("Iniciando escaneo de puertos...");
-        for(int i = 1; i <= 1024; i++) {
+        for(int i = 1; i <= 10000; i++) {
             try {
                 Socket localConn = new Socket("localhost", i);
                 System.out.println("El puerto " + i + " está abierto.");
